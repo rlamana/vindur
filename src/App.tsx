30dd9bba37426e3d083e8130df9371cda15e1663
Ramón Lamana <rlamana@gmail.com>
2023-11-24 00:38:42 +0100

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import { Typography, List, ListItem, ListItemText } from '@mui/material';
+
+interface WeatherData {
+  data: {
+    timelines: {
+      intervals: {
+        startTime: string;
+        values: {
+          windSpeed: number;
+          windDirection: number;
+        };
+      }[];
+    }[];
+  };
+}
+
+const App: React.FC = () => {
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [location, setLocation] = useState<GeolocationCoordinates | null>(null);
+  const API_KEY = 'Cldy6unrJiv47zNSnnkhvi9PP2R403uY';
+
+  useEffect(() => {
+    const fetchLocation = () => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation(position.coords);
+        },
+        (error) => {
+          console.error('Error getting location:', error);
+        }
+      );
+    };
+
+    fetchLocation();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!location) {
+        return;
+      }
+
+      try {
+        const response = await fetch(
+          `https://api.tomorrow.io/v4/timelines?location=${location.latitude},${location.longitude}&fields=windSpeed,windDirection&apikey=${API_KEY}`
+        );
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch weather data');
+        }
+
+        const data: WeatherData = await response.json();
+        setWeatherData(data);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
+
+    fetchData();
+  }, [API_KEY, location]);
+console.log(weatherData);
+  const timeline = weatherData ? weatherData.data.timelines[0] : null;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography variant="h4">7-Day Wind Conditions</Typography>
+      {timeline ? (
+        <List>
+          <React.Fragment>
+            {timeline.intervals.map((interval) => (
+              <ListItem key={interval.startTime}>
+                <ListItemText
+                  primary={`Date: ${new Date(interval.startTime).toLocaleDateString()}`}
+                  secondary={`Wind Speed: ${interval.values.windSpeed} m/s | Wind Direction: ${interval.values.windDirection}°`}
+                />
+              </ListItem>
+            ))}
+          </React.Fragment>
+        </List>
+      ) : (
+        <Typography>Loading...</Typography>
+      )}
     </div>
   );
-}
+};
 
 export default App;
